d77c8184f78b20b03aa86adc6a61a1c674f9d919
Stephen Zhou <step.zhou@gmail.com>
2013-12-03 19:43:07 -0500

    save cookie proof of concept and some minor commenting

--- js/view.js
+++ js/view.js
@@ -2,6 +2,9 @@ var _markerID = 0;
 var history = {};
 var currentItinerary = {};
 
+/**
+ * Itinerary view prototype
+ */
 function View(apiKey, secretKey, apiUrl, authUrl, cloudmadeKey) {
     this.foursquare = new Foursquare(apiKey, secretKey, apiUrl, authUrl);
     this.map = new L.map('map')
@@ -17,8 +20,12 @@ function View(apiKey, secretKey, apiUrl, authUrl, cloudmadeKey) {
     this.markerLayer = new L.layerGroup();
     this.markerLayer.addTo(map);
     this.searchForm();
+    this.saveHook();
 }
 
+/**
+ * Adds markers upon search. Defaults to New York if no location is given
+ */
 View.prototype.searchForm = function() {
     var that = this;
     $('#search-form').submit(function () {
@@ -51,18 +58,27 @@ View.prototype.searchForm = function() {
     });
 }
 
+/**
+ * Draws markers given a list of venues
+ */
 View.prototype.drawMarkers = function(venue) {
     this.markerLayer.clearLayers();
     var center = this.map.getCenter();
     this.foursquare.searchVenues(center.lat, center.lng, venue, bind(this.onVenues, this));
 }
 
+/**
+ * Iterates through venue results to add venue marker
+ */
 View.prototype.onVenues = function(venues) {
     for (var i = 0; i < venues.length; i++) {
         this.foursquare.getVenueInformation(venues[i].id, bind(this.addVenueMarker, this));        
     }
 }
 
+/**
+ * Adds venue markers to the map
+ */
 View.prototype.addVenueMarker = function(venue) {
     var latLng = new L.LatLng(venue.location.lat, venue.location.lng);
     var venue_name = venue.name;
@@ -91,12 +107,25 @@ View.prototype.addVenueMarker = function(venue) {
     this.markerLayer.addLayer(marker);
 }
 
+/**
+ * Set up save button click
+ */
+View.prototype.saveHook = function() {
+    $('#save').on('click', this.saveItinerary);
+}
+
+View.prototype.saveItinerary = function() {
+    console.log(JSON.stringify(currentItinerary));
+    $.cookie.json = true;
+    $.cookie('dummy', JSON.stringify(currentItinerary));
+    console.log(JSON.parse($.cookie('dummy')));
+}
+
 // TODO: Make object to hold this information
 function addToItinerary(venueID) {
     $(".sortable").append("<li draggable='true' id=" + venueID + ">" + history[venueID].name);
     $(".sortable").sortable();
     currentItinerary[venueID] = history[venueID]; // adds selected venue to array 
-    console.log(currentItinerary);
 }
 
 $(function() {
@@ -104,116 +133,5 @@ $(function() {
              "https://foursquare.com/", "https://api.foursquare.com",
              cloudmade_key);
 });
-
-
-//-------------------FILE SYSTEM API CODE-----------------------//
-
-
-//TEMPORARY STORAGE - HTML5ROCKS.COM
-//window.requestFileSystem(window.TEMPORARY, 5*1024*1024 /*5MB*/, onInitFs, errorHandler);
-
-function onInitFs(fs) {
-    console.log('Opened file system: ' + fs.name);
-    console.log(window.webkitStorageInfo.queryUsageAndQuota);
-    // fs.root.getFile('log.txt', {create: true, exclusive: true}, function(fileEntry) {
-
-    // // fileEntry.isFile === true
-    // // fileEntry.name == 'log.txt'
-    // // fileEntry.fullPath == '/log.txt' 
-    // }, onError);
-    fs.root.getFile('log.txt', {create: true}, function(fileEntry) {
-
-        // Create a FileWriter object for our FileEntry (log.txt).
-        fileEntry.createWriter(function(fileWriter) {
-
-          fileWriter.onwriteend = function(e) {
-            console.log('Write completed.');
-          };
-
-          fileWriter.onerror = function(e) {
-            console.log('Write failed: ' + e.toString());
-          };
-
-          // Create a new Blob and write it to log.txt.
-          var blob = new Blob(['Lorem Ipsum'], {type: 'text/plain'});
-
-          fileWriter.write(blob);
-
-        }, onError);
-
-    }, onError);
-
-}
-function errorHandler(e) {
-    var msg = '';
-
-    switch (e.code) {
-    case FileError.QUOTA_EXCEEDED_ERR:
-        msg = 'QUOTA_EXCEEDED_ERR';
-        break;
-    case FileError.NOT_FOUND_ERR:
-        msg = 'NOT_FOUND_ERR';
-        break;
-    case FileError.SECURITY_ERR:
-        msg = 'SECURITY_ERR';
-        break;
-    case FileError.INVALID_MODIFICATION_ERR:
-        msg = 'INVALID_MODIFICATION_ERR';
-        break;
-    case FileError.INVALID_STATE_ERR:
-        msg = 'INVALID_STATE_ERR';
-        break;
-    default:
-        msg = 'Unknown Error';
-    break;
-};
-
-  console.log('Error: ' + msg);
-}
-
-//PERSISTENT STORAGE - HTML5ROCKS.COM
-// This does not work, 'window.webkitStorageInfo' is deprecated. '
-// Chrome indicated to use 'navigator.webkitTemporaryStorage' or 'navigator.webkitPersistentStorage' instead.
-// window.webkitStorageInfo.requestQuota(PERSISTENT, 1024*1024, function(grantedBytes) {
-//   window.requestFileSystem(PERSISTENT, grantedBytes, onInitFs, errorHandler);
-// }, function(e) {
-//   console.log('Error', e);
-// });
-
-//This is edited code, merged with solution found on StackOverflow
-navigator.webkitPersistentStorage.requestQuota(1024*1024, function(grantedBytes) {
-    console.log('requestQuota: ' , arguments);
-    requestFS(grantedBytes);
-}, onError);
-
-function requestFS(grantedBytes) {
-    console.log("Inside requestFS");
-    window.webkitRequestFileSystem(window.PERSISTENT, grantedBytes, function(fs) {
-        console.log ('fs: ', arguments); // I see this on Chrome 27 in Ubuntu
-    }, onError);
-}
-
-//WEBKIT VERSION - StackOverflow
-//(1)
-// function onError () { console.log ('Error : ', arguments); }
-//(2)
-//function onError () { console.log ('Error ' + arguments[0].code + ": " + arguments[0].message); }
-//(3)
-function onError () { console.log ('Error ' + arguments + arguments[0].name + ": " + arguments[0].message); }
-
-// function onError () { console.log ('Error : ', arguments); }
-
-
-//  
-// navigator.webkitPersistentStorage.requestQuota (1024*1024*1024, function(grantedBytes) {
-//   console.log ('requestQuota: ', arguments);
-//   requestFS(grantedBytes);
-// }, onError);
-
-// function requestFS(grantedBytes) {
-//   window.webkitRequestFileSystem(window.PERSISTENT, grantedBytes, function(fs) {
-//     console.log ('fs: ', arguments); // I see this on Chrome 27 in Ubuntu
-//   }, onError);
-// }
 
 
