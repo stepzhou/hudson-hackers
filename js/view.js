3374bee05de15d84aae1fe69cfd6e43b9e62378b
Emily Schultz <emily.schultz@newscred.com>
2013-12-06 23:36:06 -0500

    Splash page to view page functionality

--- js/view.js
+++ js/view.js
@@ -7,11 +7,13 @@ var currentItinerary = {};
  */
 function View(apiKey, secretKey, apiUrl, authUrl, cloudmadeKey) {
     this.foursquare = new Foursquare(apiKey, secretKey, apiUrl, authUrl);
+
     this.map = new L.map('map', {
         layers: MQ.mapLayer(),
         center: [40.78, -73.97],
         zoom: 13
     });
+
     var map = this.map;
     var baseLayer = L.tileLayer('http://{s}.tile.cloudmade.com/{key}/{styleId}/256/{z}/{x}/{y}.png', {
         key: cloudmadeKey,
@@ -33,6 +35,32 @@ function View(apiKey, secretKey, apiUrl, authUrl, cloudmadeKey) {
     this.hideHook();
     this.expandHook();
     this.preloadForm();
+    this.splashForm();
+}
+
+View.prototype.splashForm = function() {
+    var text = window.location.search.substr(1).split("&");
+    if(text[0].length > 0) {
+        var venue_string = text[0];
+        var location_string = text[1];
+
+        // get venue
+        var venue = venue_string.split("=")[1];
+
+        // get location
+        if(!location_string) { 
+            var location = 'New York'; // default to New York if no location given
+        } else { 
+            var location = location_string.split("=")[1];
+        }
+        
+        var that = this;
+        this.foursquare.geocode(location, function(reply) {
+            var locCenter = reply[0]['feature']['geometry']['center'];
+            that.map.setView(locCenter, 13);
+            that.drawMarkers(venue);
+        });
+    }
 }
 
 View.prototype.preloadForm = function() { 
